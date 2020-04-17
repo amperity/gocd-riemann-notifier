965f6f0001dbf253802f583384021b1513d46ba8
Drew Inglis <drew@drewinglis.com>
2020-04-16 21:34:43 -0700

    Only set basic auth HTTP header on requests to GoCD server
    Previously, this was setting the basic auth header with the plugin's
    provided username and password for all HTTP requests that the GoCD
    server JVM was making. This has the possibility to leak the credentials
    externally.
    
    This change makes it so that we explicitly set the basic auth header on
    all requests (instead of setting the default Authentication mechanism),
    and ensures that it is only set on requests to localhost.

--- src/main/java/com/rsr5/gocd/riemann_notifier/RetrievePipelineRSS.java
+++ src/main/java/com/rsr5/gocd/riemann_notifier/RetrievePipelineRSS.java
@@ -1,38 +1,22 @@
 package com.rsr5.gocd.riemann_notifier;
 
 import java.io.IOException;
-import java.net.Authenticator;
 import java.net.HttpURLConnection;
-import java.net.PasswordAuthentication;
-import java.net.URL;
 
 public class RetrievePipelineRSS {
+    private GoApiAccessor accessor = null;
 
-    PluginConfig pluginConfig = null;
-    String username = null;
-    String password = null;
+    public RetrievePipelineRSS(GoApiAccessor accessor) {
+        this.accessor = accessor;
+    }
 
     public RetrievePipelineRSS() {
-        pluginConfig = new PluginConfig();
-        username = pluginConfig.getUsername();
-        password = pluginConfig.getPassword();
+        this(new GoApiAccessor());
     }
 
     public HttpURLConnection download() throws IOException {
-        String sURL = "http://localhost:8153/go/cctray.xml";
-
-        if (username != null && password != null){
-            Authenticator.setDefault(new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(username,
-                            password.toCharArray());
-                }
-            });
-        }
-
-        URL url = new URL(sURL);
-        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-        request.connect();
-        return request;
+        HttpURLConnection con = accessor.getConnection("/go/cctray.xml");
+        con.connect();
+        return con;
     }
 }
