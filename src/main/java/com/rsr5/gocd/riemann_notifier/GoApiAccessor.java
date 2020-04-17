09e3c97e006ede834df17b16694b4020d4cbb798
Drew Inglis <drew@drewinglis.com>
2020-04-17 10:41:58 -0700

    Only set basic auth HTTP header on requests to GoCD server
    Previously, this was setting the basic auth header with the plugin's
    provided username and password for all HTTP requests that the GoCD
    server JVM was making. This has the possibility to leak the credentials
    externally.
    
    This change makes it so that we explicitly set the basic auth header on
    all requests (instead of setting the default Authentication mechanism),
    and ensures that it is only set on requests to localhost.

--- src/main/java/com/rsr5/gocd/riemann_notifier/GoApiAccessor.java
+++ src/main/java/com/rsr5/gocd/riemann_notifier/GoApiAccessor.java
@@ -7,13 +7,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Base64;
 
 public class GoApiAccessor {
+    private PluginConfig pluginConfig = null;
+
+    public GoApiAccessor(PluginConfig pluginConfig) {
+        this.pluginConfig = pluginConfig;
+    }
+
+    public GoApiAccessor() {
+        this(new PluginConfig());
+    }
+
     public JsonElement get(String path) throws IOException {
-        URL url = new URL("http", "localhost", 8153, path);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        HttpURLConnection con = this.getConnection(path);
         con.setRequestProperty("Accept", "application/vnd.go.cd+json");
         con.connect();
         return JsonParser.parseReader(new InputStreamReader(con.getInputStream()));
     }
+
+    public HttpURLConnection getConnection(String path) throws IOException {
+        URL url = new URL("http", "localhost", 8153, path);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        if (this.authRequired()) {
+            con.setRequestProperty("Authorization", this.getAuthHeader());
+        }
+        return con;
+    }
+
+    private boolean authRequired() {
+        return this.pluginConfig.getUsername() != null && this.pluginConfig.getPassword() != null;
+    }
+
+    private String getAuthHeader() {
+        String authString = this.pluginConfig.getUsername() + ":" + this.pluginConfig.getPassword();
+        return Base64.getEncoder().encodeToString(authString.getBytes());
+    }
+
 }
